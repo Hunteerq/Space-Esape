abd81ae56e01dad5c11764af456be2b549aead48
Artur Kulig <hunteerq@icloud.com>
2019-05-11 15:25:59 +0200

    Initial commit with menu, and basic ship movement

---
+++ Assets/Scripts/menu/ChangeSceneHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneHandler : MonoBehaviour {
+
+    public void OnClick(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
